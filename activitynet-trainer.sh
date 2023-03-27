426d184e919e9ab9688e32c070b4adc198e41369
Hans7331 <hans.tiwari@gmail.com>
2023-03-27 16:57:34 +0300

    pr change

--- activitynet-trainer.sh
+++ activitynet-trainer.sh
@@ -6,4 +6,4 @@
 #SBATCH --output=../anet_output/activitynet_output_module_3_test.out
 
 module load anaconda
-srun python trainer.py --annot_dir "../anet_annotations/activitynet-3-category.json" --num_classes 3 --epochs 3 --lr 0.1 --dataset "Anet" --batch_size 32 --frames_per_clip 32 --pr 6
+srun python trainer.py --annot_dir "../anet_annotations/activitynet-3-category.json" --num_classes 3 --epochs 3 --lr 0.1 --dataset "Anet" --batch_size 32 --frames_per_clip 32 --pr 0
