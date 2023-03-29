2c58f6fba60246d4efaf14d2496edc146f2f159e
Hans7331 <hans.tiwari@gmail.com>
2023-03-29 16:32:06 +0300

    shape change

--- activitynet-trainer.sh
+++ activitynet-trainer.sh
@@ -6,4 +6,4 @@
 #SBATCH --output=../anet_output/e50-lr-0.0001-all-pretrain-fixed_test.out
 
 module load anaconda
-srun python trainer.py --annot_dir "../anet_annotations/activitynet-3-category.json" --num_classes 3 --epochs 1 --lr 0.1 --dataset "Anet" --batch_size 32 --frames_per_clip 16 --pr 2
+srun python trainer.py --annot_dir "../anet_annotations/activitynet-3-category.json" --num_classes 3 --epochs 1 --lr 0.1 --dataset "Anet" --batch_size 32 --frames_per_clip 32 --pr 2
