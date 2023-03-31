c189d5eaceac4ba69d8f9166f06ef1d2616f6ff4
Hans7331 <hans.tiwari@gmail.com>
2023-03-31 18:47:38 +0300

    sh update + tube true

--- activitynet-trainer.sh
+++ activitynet-trainer.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=90:00:00
 #SBATCH --cpus-per-task=15
 #SBATCH --mem=128G
-#SBATCH --output=../anet_output/e50-lr0.000001_do_test.out
+#SBATCH --output=../anet_output/e50-lr0.000001_do-added_tube-True_test.out
 
 module load anaconda
 srun python trainer.py --annot_dir "../anet_annotations/activitynet-3-category.json" --num_classes 3 --epochs 1 --lr 0.00001 --dataset "Anet" --batch_size 32 --frames_per_clip 32 --pr 2
