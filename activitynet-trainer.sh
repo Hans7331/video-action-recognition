b791334f828a0ad2975cc9e42c196886fd83a9a7
Hans7331 <hans.tiwari@gmail.com>
2023-04-14 05:47:09 +0300

    test

--- activitynet-trainer.sh
+++ activitynet-trainer.sh
@@ -6,4 +6,4 @@
 #SBATCH --output=../anet_output/bs8_200_lr0.000001_imagenet.out
 
 module load anaconda
-srun python trainer.py --annot_dir "../anet_annotations/activitynet-200-category.json" --num_classes 200 --epochs 50 --lr 0.000001 --dataset "Anet" --batch_size 8 --frames_per_clip 32 --pr 2 --tt_split 0.9
+srun python trainer.py --annot_dir "../anet_annotations/activitynet-3-category.json" --num_classes 3 --epochs 3 --lr 0.01 --dataset "Anet" --batch_size 8 --frames_per_clip 32 --pr 2 --tt_split 0.9
--- trainer.py
+++ trainer.py
@@ -222,7 +222,7 @@ if opt.pr == 1:
 elif opt.pr == 0:
     model = model_2_scratch(image_size= opt.image_size, patch_size=patch_size, num_classes=num_classes, frames_per_clip=frames_per_clip)
 elif opt.pr == 2:
-    model = model_2_pretrained_all_layers(image_size= opt.image_size, patch_size=patch_size, num_classes=num_classes, frames_per_clip=frames_per_clip, tube = False, dropout = 0.1 ,emb_dropout = 0.1 )
+    model = model_2_pretrained_all_layers(image_size= opt.image_size, patch_size=patch_size, num_classes=num_classes, frames_per_clip=frames_per_clip, tube = False, dropout = 0 ,emb_dropout = 0 )
 
 # if using the all layered pretriained model
 
@@ -277,13 +277,13 @@ for epoch in tqdm(range(1,epochs+1)):
 
     print("validation cm diagonal: ",np.diag(np.array(confusion_matrix)))
     print("test cm diagonal: ",np.diag(np.array(confusion_matrix_test)))
-    #print("test cm sum: ",np.sum(np.array(confusion_matrix_test), axis = 1))
-    #print("test cm sum: ",np.sum(np.array(confusion_matrix_test), axis = 0))
+    print("test cm sum: ",np.sum(np.array(confusion_matrix_test), axis = 1))
+    print("test cm sum: ",np.sum(np.array(confusion_matrix_test), axis = 0))
 
 print("validation cm diagonal: ",np.diag(np.array(confusion_matrix)))
 print("test cm diagonal: ",np.diag(np.array(confusion_matrix_test)))
-#print("test cm sum: ",np.sum(np.array(confusion_matrix_test), axis = 1))
-#print("test cm sum: ",np.sum(np.array(confusion_matrix_test), axis = 0))    
+print("test cm sum: ",np.sum(np.array(confusion_matrix_test), axis = 1))
+print("test cm sum: ",np.sum(np.array(confusion_matrix_test), axis = 0))    
 
 torch.save(model,"../model_save/vivit-last-model.pt")
 torch.save(model.state_dict(), '../model_save/vivit-last-model-parameters.pt')
