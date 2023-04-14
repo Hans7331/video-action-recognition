2c83e91a5da61b2ba295887cfb2788c73c7ad0ea
Hans7331 <hans.tiwari@gmail.com>
2023-04-14 05:50:59 +0300

    test

--- activitynet-trainer.sh
+++ activitynet-trainer.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=90:00:00
 #SBATCH --cpus-per-task=15
 #SBATCH --mem=128G
-#SBATCH --output=../anet_output/bs8_200_lr0.000001_imagenet.out
+#SBATCH --output=../anet_output/test.out
 
 module load anaconda
 srun python trainer.py --annot_dir "../anet_annotations/activitynet-3-category.json" --num_classes 3 --epochs 3 --lr 0.01 --dataset "Anet" --batch_size 8 --frames_per_clip 32 --pr 2 --tt_split 0.9
