56d81b1fc0c9613e3162589237d80b0d114b6384
Hans7331 <hans.tiwari@gmail.com>
2023-04-14 15:53:41 +0300

    test

--- activitynet-trainer.sh
+++ activitynet-trainer.sh
@@ -6,4 +6,4 @@
 #SBATCH --output=../anet_output/updated__test_stride_1.out
 
 module load anaconda
-srun python trainer.py --annot_dir "../anet_annotations/activitynet-3-category.json" --num_classes 3 --epochs 1 --lr 0.000001 --dataset "Anet" --batch_size 16 --frames_per_clip 32 --pr 2 --tt_split 0.9 --str 1
+srun python trainer.py --annot_dir "../anet_annotations/activitynet-3-category.json" --num_classes 3 --epochs 1 --lr 0.000001 --dataset "Anet" --batch_size 16 --frames_per_clip 32 --pr 2 --tt_split 0.9 --str 2
