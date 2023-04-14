2ff3d64bdd6c3bbe6429bc072d8843baa94167d5
Hans7331 <hans.tiwari@gmail.com>
2023-04-14 23:25:01 +0300

    test

--- activitynet-trainer.sh
+++ activitynet-trainer.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=90:00:00
 #SBATCH --cpus-per-task=15
 #SBATCH --mem=128G
-#SBATCH --output=../anet_output/UPDATED_lr0.00001_bs16_stride_2_do0.out
+#SBATCH --output=../anet_output/UPDATED_lr0.1_bs32_stride_2_do0.out
 
 module load anaconda
-srun python trainer.py --annot_dir "../anet_annotations/activitynet-200-category.json" --num_classes 200 --epochs 50 --lr 0.00001 --dataset "Anet" --batch_size 16 --frames_per_clip 32 --pr 2 --tt_split 0.9 --str 2 --dropout 0
+srun python trainer.py --annot_dir "../anet_annotations/activitynet-200-category.json" --num_classes 200 --epochs 50 --lr 0.1 --dataset "Anet" --batch_size 32 --frames_per_clip 32 --pr 2 --tt_split 0.9 --str 2 --dropout 0
